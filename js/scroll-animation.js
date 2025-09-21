bbff3221ed7a6a25c2a150f110d229e4d181d3db
Fabricio <50778242+fabriciosouza0@users.noreply.github.com>
2025-09-21 09:23:04 -0300

    feat: add animation on scroll.

---
+++ js/scroll-animation.js
@@ -0,0 +1,21 @@
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("animate__fadeOut");
+        entry.target.classList.add("animate__fadeIn");
+      } else {
+        entry.target.classList.remove("animate__fadeIn");
+        entry.target.classList.add("animate__fadeOut");
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+const elementsToAnimate = document.querySelectorAll(".animate__animated");
+elementsToAnimate.forEach((element) => {
+  observer.observe(element);
+});
